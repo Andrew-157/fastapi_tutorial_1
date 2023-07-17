40cfb77a579092571226f54313abe11d5978d83e
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-17 13:06:13 +0200

    Number validators

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ app = FastAPI()
 
 @app.get("/items/{item_id}/")
 async def read_items(
-    item_id: Annotated[int, Path(title="The ID of the item to get")],
-    q: Annotated[str | None, Query(alias="item-query")] = None
+        item_id: Annotated[int, Path(title="The ID of the item to get", ge=1)],
+        q: str Query()
 ):
+    """
+    le: less then or equal 
+    lt: less than
+    gt: greater than
+    can also be applied, and not only in Path,
+    but also in Query
+    """
     results = {"item_id": item_id}
     if q:
         results.update({"q": q})
