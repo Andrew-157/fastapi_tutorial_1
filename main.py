f657b57c88a813adec8a0c8bd276fae984f359b0
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-17 10:27:06 +0200

    Multiple path and query parameters

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/items/{item_id}/")
-async def read_items(item_id: str, q: str | None = None, short: bool = False):
-    item = {"item_id": item_id}
+@app.get("/users/{user_id}/items/{item_id}/")
+async def read_user_id(user_id: int, item_id: int, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
     if q:
         item.update({"q": q})
     if not short:
