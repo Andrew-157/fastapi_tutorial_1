c3832ca7e53a7d51ba0fa031c963e43cc76ea9f9
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-17 10:12:52 +0200

    Path parameters with optional query parameters

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-fake_items_db = [{"item_name": "Foo"}, {
-    "item_name": "Bar"}, {"item_name": "Baz"}]
 
-
-@app.get("/items/")
-async def read_items(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip: skip + limit]
+@app.get("/items/{item_id}/")
+async def read_items(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    else:
+        return {"item_id": item_id}
