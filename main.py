dd28814886cade71715cd2a050ad08c53daec850
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-17 14:58:43 +0200

    Bodies of arbitary dicts

--- main.py
+++ main.py
@@ -8,11 +8,6 @@ from pydantic import BaseModel, HttpUrl
 app = FastAPI()
 
 
-class Image(BaseModel):
-    url: HttpUrl
-    name: str
-
-
-@app.post("/images/multiple/")
-async def create_multiple_images(images: list[Image]):
-    return images
+@app.post("/index-weights/")
+async def create_index_weights(weights: dict[int, float]):
+    return weights
