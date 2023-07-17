4a3b048ac7097a086fdb56af3b37c06490589a0e
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-17 13:21:54 +0200

    Mix Path, Query and path params

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 from typing import Annotated
+
 from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
 
-@app.get("/items/{item_id}/")
-async def read_items(
-        item_id: Annotated[int, Path(title="The ID of the item to get", ge=1)],
-        q: str Query()
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float
+
+
+@app.put("/items/{item_id}")
+async def update_item(
+        item_id: Annotated[int, Path(
+            title="The ID of the item to get", ge=0, le=1000)],
+        q: str,
+        item: Item | None = None,
 ):
-    """
-    le: less then or equal 
-    lt: less than
-    gt: greater than
-    can also be applied, and not only in Path,
-    but also in Query
-    """
     results = {"item_id": item_id}
     if q:
         results.update({"q": q})
+    if item:
+        results.update({"item": item})
     return results
