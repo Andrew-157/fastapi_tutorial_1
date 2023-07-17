eefc3ef8532d67677a2545ec452802a6c7d94238
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-17 10:35:48 +0200

    Required query params

--- main.py
+++ main.py
@@ -4,13 +4,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/users/{user_id}/items/{item_id}/")
-async def read_user_id(user_id: int, item_id: int, q: str | None = None, short: bool = False):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
+@app.get("/items/{item_id}/")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
     return item
