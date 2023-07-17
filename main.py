8ac9532d25167ce972b97b89863438e0fef8baee
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-17 12:24:35 +0200

    Query parameter list / multiple values with defaults

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: Annotated[list[str] | None, Query()]):
+async def read_items(q: Annotated[list[str] | None, Query()] = ["foo", "bar"]):
     query_items = {"q": q}
     return query_items
