8d9ca5c780af570b06b3a36908fea20cdc9b8736
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-18 14:59:11 +0200

    Path operation function return type

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from typing import Annotated
-
-from fastapi import FastAPI, Header
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: list[str] = []
+
+
+@app.post("/items/")
+async def create_item(item: Item) -> Item:
+    return item
+
+
 @app.get("/items/")
-async def read_items(x_token: Annotated[list[str] | None, Header()] = None):
-    return {"X-token values": x_token}
+async def read_items() -> list[Item]:
+    return [
+        Item(name='Portal Gun', price=42.0),
+        Item(name='Plumb', price=32.0)
+    ]
