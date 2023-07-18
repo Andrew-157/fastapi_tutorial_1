31fe38b2cfc7d06b98522c47fb5700162a82839c
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-18 14:20:49 +0200

    Body with examples

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Body
 from pydantic import BaseModel, Field
 
 
@@ -6,13 +8,36 @@ app = FastAPI()
 
 
 class Item(BaseModel):
-    name: str = Field(examples=['Foo'])
-    description: str | None = Field(default=None, examples=['Very nice item'])
-    price: float = Field(examples=[35.4])
-    tax: float | None = Field(default=None, examples=[3.2])
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
 @app.put("/items/{item_id}/")
-async def update_item(item_id: int, item: Item):
+async def update_item(
+    item_id: int,
+    item: Annotated[
+        Item,
+        Body(
+            examples=[
+                {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                },
+                {
+                    "name": "Bar",
+                    "price": "35.4",
+                },
+                {
+                    "name": "Baz",
+                    "price": "thirty five point four",
+                },
+            ],
+        )
+    ]
+):
     results = {"item_id": item_id, "item": item}
     return results
