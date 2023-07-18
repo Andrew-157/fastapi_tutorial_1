d01c1a6dfa497adaccd76edfcd6c1370b9a7e7ca
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-18 14:09:24 +0200

    Extra JSON Schema in Pydantic models

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from typing import Annotated
-
-
-from fastapi import FastAPI, Query, Path, Body
-from pydantic import BaseModel, HttpUrl
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
-@app.post("/index-weights/")
-async def create_index_weights(weights: dict[int, float]):
-    return weights
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "name": "Foo",
+                    "description": "A very nice item.",
+                    "price": 35.4,
+                    "tax": 3.2
+                }
+            ]
+        }
+    }
+
+
+@app.put("/items/{item_id}/")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results
