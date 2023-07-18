03812189a363b328007345fa30bb74c9e303331c
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-18 14:48:10 +0200

    Dealing with Duplicate headers

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(user_agent: Annotated[str | None, Header()] = None):
-    return {"User-Agent": user_agent}
+async def read_items(x_token: Annotated[list[str] | None, Header()] = None):
+    return {"X-token values": x_token}
