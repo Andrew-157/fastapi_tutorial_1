77fc12d9dd993af4f86872f3ffc8585db87a937e
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-23 17:22:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from typing import Annotated
-from fastapi import FastAPI, Form
 
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer
 
 app = FastAPI()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-@app.post("/login/")
-async def login(username: Annotated[str, Form()],
-                password: Annotated[str, Form()]):
-    return {"username": username}
+
+@app.get("items")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
